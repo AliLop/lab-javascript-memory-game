d676a9b3da25795010ca92f6cbc5be4fbf3e6090
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-10-25 18:26:31 +0000

    almost done

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ const cards = [
   { name: 'superman', img: 'superman.jpg' },
   { name: 'the avengers', img: 'the-avengers.jpg' },
   { name: 'thor', img: 'thor.jpg' },
+    //Repeat
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
@@ -24,10 +25,13 @@ const cards = [
   { name: 'the avengers', img: 'the-avengers.jpg' },
   { name: 'thor', img: 'thor.jpg' }
 ];
-
+//console.log(cards);
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
+
+  memoryGame.shuffleCards(cards);
+
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -35,15 +39,87 @@ window.addEventListener('load', event => {
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
+  // (e)   function(e) {  }
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
+    
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      // NOT ADDING THE TURNED 
+      /*if (!card.classList.contains('turned')) {
+        card.classList.add('turned');
+      } else {
+         card.classList.remove('turned');
+      }  // 1st click adds turned and turns the card 2 twice
+      // 2nd click does not work */
+      
+      card.classList.add('turned'); // turns card twice
+
+      card.querySelectorAll('.card div').forEach(div => {
+         div.classList.toggle('front');
+         div.classList.toggle('back');
+      });
+
       console.log(`Card clicked: ${card}`);
+      
+       // NOT LIMITTING TO A MAX OF 2 CARDS FLIPPED 
+       //for (let i = 0; memoryGame.pickedCards.length < 2; i++) { NOt WORKING }
+      memoryGame.pickedCards.push(card);               
+
+      if (memoryGame.pickedCards.length === 2) {
+      const card1 = memoryGame.pickedCards[0];
+      const card2 = memoryGame.pickedCards[1];
+      //card1.classList.add('turned');
+      //card2.classList.add('turned');
+      //console.log(memoryGame.pickedCards);
+
+        if(memoryGame.checkIfPair(card1, card2)) {
+         
+          document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+          console.log(memoryGame.pairsGuessed);
+          
+          card1.classList.add('blocked');
+          card2.classList.add('blocked');
+
+          memoryGame.pickedCards = [];
+
+          memoryGame.isFinished();
+
+        } else {
+          //document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          //console.log(memoryGame.pairsClicked);
+
+          setTimeout(() => {
+            memoryGame.pickedCards.forEach(card => { 
+              card.querySelectorAll('.card div').forEach(div => {
+                div.classList.toggle('front');
+                div.classList.toggle('back');
+              });
+            });
+            card1.classList.remove('turned');
+            card2.classList.remove('turned');
+            memoryGame.pickedCards = [];
+          }, 1000) 
+        };
+        console.log(memoryGame.pickedCards); 
+
+        
+        document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+        console.log(memoryGame.pairsClicked);
+      }
+      
+      if (memoryGame.isFinished()) {
+        setTimeout(() => {
+          window.alert('YOU WON!')
+        }, 1000)
+      }
+      
     });
+
+
   });
 });
+//console.log(memoryGame.shuffleCards(cards));
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,50 @@
 class MemoryGame {
   constructor(cards){
-    this.cards = cards;
+    this.cards = cards;  // array we send as const
     // add the rest of the class properties here
+    this.pickedCards = []; // array to store cards form comparing 
+    this.pairsClicked = 0; // to add every time a user choose 
+    this.pairsGuessed = 0; // to add every time a user guess a pair
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    if (!cards) {   // if tehre is no array
+      return undefined
+    } else {
+      // Fisher–Yates Shuffle in JS
+      let mixed = [];
+      let n = cards.length, i;
+      
+      while(n) {  // While there remain elements to shuffle…
+        i = Math.floor(Math.random() * n--); // Pick a remaining element…
+        mixed.push(cards[i]); // And move it to the new array.
+      }
+
+      //console.log(mixed);
+      return this.cards = mixed; // should return the shuffled (mixed) array of cards
+    
+    }
+  } 
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    
+    if (card1.getAttribute('data-card-name') === card2.getAttribute('data-card-name')) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    //should return false at the beginning of the game
+    // should return false if there's still some pairs to be guessed
+    if (this.pairsGuessed === 0 || this.pairsGuessed < this.cards.length / 2) {
+        return false; 
+    } else if (this.pairsGuessed === this.cards.length / 2) {
+        return true;
+    //should return true if all pairs are guessed 
+    // actually it should consider it depending on th length of teh array 
+    //(test considers this at #8 but it should be at 12 since tehre are 24 cards in total)
+    }
+  }
 }
